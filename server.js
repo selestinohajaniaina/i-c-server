c999ed83e5510c36303656f114c168020a07a372
Selestino <selestinoolivier@gmail.com>
2023-03-26 14:18:20 +0200

    second commit

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ server.use(cors({
 
 server.listen(3000,console.log('server start on 3000'))
 
+//selection tout par table
+
 server.get("/select/:table", (req, res) => {
   let table = req.params.table;
   var sql = `SELECT * FROM ${table} `;
@@ -38,7 +40,7 @@ server.get("/select/:table", (req, res) => {
   });
 });
 
-//insertion de categorie
+//insertion de categorie dans le table categorie
  
 server.post("/insert/categorie", (req, res) => {
   let details = {
@@ -55,7 +57,7 @@ server.post("/insert/categorie", (req, res) => {
   });
 });
 
-//supression du categorie
+//supression du categorie dans le table categorie
 
 server.delete("/delete/categorie/:id", (req, res) => {
   let sql = "DELETE FROM categorie WHERE cat_id=" + req.params.id + "";
@@ -68,7 +70,7 @@ server.delete("/delete/categorie/:id", (req, res) => {
   });
 });
 
-//insertion de produit
+//insertion de produit dans le table produit
 
 server.post("/insert/produit", (req, res) => {
   let detailsPro = {
@@ -86,7 +88,7 @@ server.post("/insert/produit", (req, res) => {
   });
 });
 
-//supression du produit
+//supression du produit dans le table produit
 
 server.delete("/delete/produit/:id", (req, res) => {
   let sql = "DELETE FROM produit WHERE codePro=" + req.params.id + "";
@@ -114,3 +116,120 @@ server.post("/insert/recette", (req, res) => {
     }
   });
 });
+
+//selection nom du plat 
+
+server.get("/select/plat/:id_plat", (req, res) => {
+  let id_plat = req.params.id_plat;
+  var sql = `SELECT * FROM plat WHERE id_plat=${id_plat} `;
+  db.query(sql, function (error, result) {
+    if (error) {
+      console.log("Error Connecting to DB");
+    } else {
+      res.send( {result} );
+    }
+  });
+});
+
+//selection des produits dans un plat 
+
+server.get("/select/detail/:id_plat", (req, res) => {
+  let id_plat = req.params.id_plat;
+  var sql = `SELECT * FROM detailplat WHERE id_plat=${id_plat} ORDER BY libellePro ASC`;
+  db.query(sql, function (error, result) {
+    if (error) {
+      console.log("Error Connecting to DB");
+    } else {
+      res.send( {result} );
+    }
+  });
+});
+
+//insertion de plat (ou dite recette )
+
+server.post("/plat/add/produit", (req, res) => {
+  let detailsPro = {
+    id_plat: req.body.id_plat,
+    libellePro: req.body.libellePro,
+    variete: req.body.variete,
+    qte: req.body.qte,
+  };
+  let sql = "INSERT INTO detailplat SET ?";
+  db.query(sql, detailsPro, (error) => {
+    if (error) {
+      res.send({ status: false, message: "produit's plat created Failed" });
+    } else {
+      res.send({ status: true, message: "produit's plat created successfully" });
+    }
+  });
+});
+
+//supression d'un produit dans un recette
+
+server.delete("/delete/detail/:id", (req, res) => {
+  let sql = "DELETE FROM detailplat WHERE id=" + req.params.id + "";
+  let query = db.query(sql, (error) => {
+    if (error) {
+      res.send({ status: false, message: "Produit Deleted from detailplat Failed" });
+    } else {
+      res.send({ status: true, message: "Produit Deleted from detailplat successfully" });
+    }
+  });
+});
+
+//signup into db // insertion d'info d'utilisateur dans la base de donnée
+
+server.post("/person/add", (req, res) => {
+  let detailsUser = {
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  };
+  let sql = "INSERT INTO user SET ?";
+  db.query(sql, detailsUser, (error) => {
+    if (error) {
+      res.send({ status: false, message: "user created Failed" });
+    } else {
+      res.send({ status: true, message: "user created successfully" });
+    }
+  });
+});
+
+//selection user in db
+
+server.get("/user/:email", (req, res) => {
+  let email = req.params.email;
+  var sql = `SELECT * FROM user WHERE email='${email}'`;
+  db.query(sql, function (error, result) {
+    if (!result) {
+      res.send({ status: false, message: "compte introuvable" });
+    } else {
+      res.send({ status: true, message: "user connected successfully", data: result });
+    }
+  });
+});
+
+//insertion avis dans la base de donnée
+
+server.post("/insert/feedback", (req, res) => {
+  let detailsUser = {
+    id_plat:req.body.id_plat,
+    temps: req.body.min,
+    dificulte: req.body.dificulte,
+    etoile: req.body.rating,
+  };
+  delFeedback(req.body.id_plat);
+  let sql = "INSERT INTO avis SET ?";
+  db.query(sql, detailsUser, (error) => {
+    if (error) {
+      res.send({ status: false, message: "avis created Failed" });
+    } else {
+      res.send({ status: true, message: "avis created successfully" });
+    }
+  });
+});
+
+//suprimer avis dans la base de donnée si existe, pour inserer a nouveau
+function delFeedback(number){
+  db.query('DELETE FROM avis WHERE id_plat='+number+'');
+}
